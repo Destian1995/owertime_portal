6d80b502996c5c9df86bda9310093d5cb8081454
Destian <Lerdonia@mail.ru>
2025-10-16 21:32:53 +0400

    Доработка ЛК и оптимизация отправки писем, доработка согласования

--- project_settings/urls.py
+++ project_settings/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from overtime_app.views import home, portal_login, register
+from overtime_app.views import home, portal_login
 from overtime_app.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
     path('login/', portal_login, name='portal_login'),
-    path('register/', register, name='register'),
     path('', LogoutView.as_view(next_page='home'), name='logout'),
     path('admin/', admin.site.urls),
     path('lk/', include('overtime_app.urls')),
